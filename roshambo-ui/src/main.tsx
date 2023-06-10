d36d086062103ca1c90333289c37fe8c4489dd89
Evan Shortiss <evanshortiss@gmail.com>
2023-06-09 17:57:38 -0700

    chore: remove ref to unused provider

--- roshambo-ui/src/main.tsx
+++ roshambo-ui/src/main.tsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// import GameStateContextProvider from './GameStateProvider.tsx'
 import StateMachineContextProvider from './StateMachineProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <StateMachineContextProvider>
-      {/* <GameStateContextProvider> */}
         <App />
-      {/* </GameStateContextProvider> */}
     </StateMachineContextProvider>
   </React.StrictMode>,
 )
