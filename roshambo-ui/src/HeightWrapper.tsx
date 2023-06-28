20e6b4ef1957f73e733d3a17807bb18adbb61cad
Evan Shortiss <evanshortiss@gmail.com>
2023-06-27 17:30:59 -0700

    fix: height overflow on mobile devices

---
+++ roshambo-ui/src/HeightWrapper.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+
+const HeightWrapper: React.FC<{ children: JSX.Element }> = ({ children }) => {
+  
+  useEffect(() => {
+    function resizeListener () {
+      console.log('height', `${window.innerHeight}px`)
+      document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`)
+    }
+
+    window.addEventListener('resize', resizeListener)
+
+    resizeListener()
+
+    return () => window.removeEventListener('resize', resizeListener)
+  })
+
+  return (
+    <>{children}</>
+  )
+};
+
+export default HeightWrapper;
