e5852e053f28582b2f24c4fed68f5bc2d65cb379
Evan Shortiss <evanshortiss@gmail.com>
2023-08-09 14:04:45 -0700

    feat: handle backend uuid reuse user assignment when feasible

--- roshambo-ui/src/Api.ts
+++ roshambo-ui/src/Api.ts
@@ -1,17 +1,75 @@
+import log from 'barelog'
 
+const STORAGE_KEY = 'init-assign'
 
+export function processHeartBeat (data: SSEContentHeartbeat) {
+  const prior = getPriorInitAndAssign();
+
+  if (!prior) {
+    // Could happen if SSE connects and receives heartbeat prior to completion
+    // of initAndAssign. Messy, but this is fine for now...
+    return
+  }
+
+  if (data.uuid !== prior.config.configuration.uuid) {
+    window.location.href = '/'
+  }
+}
+
+/**
+ * This function initialises the game by retrieving a user ID, name, and team
+ * assignment. It checks localStorage for a prior assignment, and verifies that
+ * the prior game UUID matches the current UUID returned from the backend. If
+ * the UUIDs do not match it requests a new user assignment.
+ * 
+ * The backend could use probably sessions to simplify this...
+ * 
+ * @returns 
+ */
 export async function initAndAssign (): Promise<{ config: InitResponse, user: UserAssignment }> {
-  const [ config, assignment ] = await Promise.all([
-    // In development mode these requests go to the Vite development server
-    // and get proxied to the Quarkus backend. For production, we'll need
-    // to configure NGINX to proxy them to the Quarkus Pod/Service
-    fetch('/game/init').then(r => r.json() as Promise<InitResponse>),
-    fetch('/game/assign').then(r => r.json() as Promise<UserAssignment>)
-  ])
+  log('getting game configuration and user assignment')
+  const priorConfig = getPriorInitAndAssign()
 
-  return new Promise((resolve) => {
-    setTimeout(() => resolve({ config, user: assignment }), 1000);
-  })
+  if (priorConfig) {
+    const config = await fetch('/game/init').then(r => r.json() as Promise<InitResponse>)
+  
+    if (config.configuration.uuid === priorConfig.config.configuration.uuid) {
+      // The UUIDs match. We can reuse the existing assignment
+      log('reusing existing user assignment')
+      return new Promise((resolve) => {
+        setTimeout(() => resolve({ config, user: priorConfig.user }), 1000);
+      })
+    } else {
+      // UUID mismatch. Clear stored data and reinitialise
+      log('clearing old assignment data and reinitialising')
+      localStorage.clear()
+
+      return initAndAssign()
+    }
+  } else {
+    const [ config, assignment ] = await Promise.all([
+      // In development mode these requests go to the Vite development server
+      // and get proxied to the Quarkus backend. For production, we'll need
+      // to configure NGINX to proxy them to the Quarkus Pod/Service
+      fetch('/game/init').then(r => r.json() as Promise<InitResponse>),
+      fetch('/game/assign').then(r => r.json() as Promise<UserAssignment>)
+    ])
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify({config, user: assignment}))
+
+    return new Promise((resolve) => {
+      setTimeout(() => resolve({ config, user: assignment }), 1000);
+    })
+  }
+}
+
+function getPriorInitAndAssign (): undefined|{ config: InitResponse, user: UserAssignment }  {
+  try {
+    const strData = localStorage.getItem(STORAGE_KEY)
+    return strData ? JSON.parse(strData) : undefined
+  } catch (e) {
+    console.error('failed to read previous init and assign data', e)
+  }
 }
 
 
@@ -26,6 +84,7 @@ export type Config = {
   roundTimeInSeconds: number
   timeBetweenRoundsInSeconds: number
   enableCamera: boolean
+  uuid: string
 }
 
 export type InitResponse = {
@@ -77,11 +136,16 @@ export type SSEContentDisable = ScoreData&{
 
 export type SSEContentEnd = ScoreData
 
-export type SSEContent = SSEContentDisable|SSEContentEnable|SSEContentEnd
+export type SSEContentHeartbeat = {
+  uuid: string
+}
+
+export type SSEContent = SSEContentDisable|SSEContentEnable|SSEContentEnd|SSEContentHeartbeat
 
 export type SSE<E extends SSEType|unknown> = {
   type: E,
   content: E extends SSEType.Enable ? SSEContentEnable : 
             E extends SSEType.Disable ? SSEContentDisable : 
-            E extends SSEType.End ? SSEContentEnd : unknown
+            E extends SSEType.End ? SSEContentEnd :
+            E extends SSEType.Heartbeat ? SSEContentHeartbeat: unknown
 }
