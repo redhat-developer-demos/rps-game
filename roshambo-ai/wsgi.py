cb042b81c20deaf4ba76c55609b3499c4998fd3a
Evan Shortiss <evanshortiss@gmail.com>
2023-10-12 09:42:14 -0700

    feat: add logging to prediction requests

--- roshambo-ai/wsgi.py
+++ roshambo-ai/wsgi.py
@@ -1,6 +1,12 @@
 import json
+import os
+import uuid
+import logging
 from flask import Flask, jsonify, request
 from prediction import predict
+
+log_level = os.getenv("LOG_LEVEL", "INFO").upper()
+logging.basicConfig(level=log_level)
 
 application = Flask(__name__)
 
@@ -8,11 +14,18 @@ application = Flask(__name__)
 @application.route('/')
 @application.route('/status')
 def status():
+    logging.debug('responding OK to /status check')
     return jsonify({'status': 'ok'})
 
 
 @application.route('/predictions', methods=['POST'])
 def create_prediction():
+    short_uuid = uuid.uuid4().hex[:8]
+    logging.info('prediction request received (ID:' + short_uuid + ')')
+    
     data = request.data or '{}'
     body = json.loads(data)
-    return jsonify(predict(body))
+    
+    result = jsonify(predict(body))
+    logging.info('prediction request complete (ID:' + short_uuid + ')')
+    return result
