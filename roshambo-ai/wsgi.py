a81a0a9786947bbf28e6559364e1f53d8dbed5f7
Evan Shortiss <evanshortiss@gmail.com>
2023-10-12 10:17:46 -0700

    chore: revert logging

--- roshambo-ai/wsgi.py
+++ roshambo-ai/wsgi.py
@@ -7,6 +7,14 @@ from prediction import predict
 
 application = Flask(__name__)
 
+log_level = os.getenv("LOG_LEVEL", "INFO").upper()
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+application.logger.addHandler(handler)
+application.logger.setLevel(log_level)
+
 @application.route('/')
 @application.route('/status')
 def status():
